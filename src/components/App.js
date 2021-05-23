b141a454d21cb20ff6fd35dc3228028d53dccffb
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-05-23 15:01:22 +0530

    Added Header and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -9,10 +10,11 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Header</h1>
+    <div className="ui container">
+
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
