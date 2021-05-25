c76f30c7ca0eac30c731bde7464090c093d988fb
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-05-25 12:22:10 +0530

    connect to stream create page

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,23 +1,47 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { fetchStreams } from '../../actions'
 
 
-const StreamList = ({ fetchStreams, streams }) => {
+const StreamList = ({ fetchStreams, streams, currentUserId, isSignedIn }) => {
 
     useEffect(() => {
         fetchStreams()
     }, [])
 
+    const renderAdmin = (stream) => {
+        if (stream.userId === currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            )
+        }
+    }
+
+    const renderCreate = () => {
+        if (isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            )
+        }
+    }
+
     const renderList = () => {
         return streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {renderAdmin(stream)}
                     <i className="large middle aligned icon camera"></i>
                     <div className="content">
                         {stream.title}
                         <div className="description">{stream.description}</div>
                     </div>
+
                 </div>
             )
         })
@@ -28,12 +52,15 @@ const StreamList = ({ fetchStreams, streams }) => {
         <div>
             <h2>Streams</h2>
             <div className="ui celled list">{renderList()}</div>
+            {renderCreate()}
         </div>
     )
 }
 const mapStateToProps = (state) => {
     return {
-        streams: Object.values(state.streams)
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 }
 
