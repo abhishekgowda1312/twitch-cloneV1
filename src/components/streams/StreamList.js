d0f68abff4c7af660037bc092667f9702ade06ea
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-07-22 17:27:55 +0530

    completed delete action

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -15,7 +15,8 @@ const StreamList = ({ fetchStreams, streams, currentUserId, isSignedIn }) => {
             return (
                 <div className="right floated content">
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-                    <button className="ui button negative">Delete</button>
+
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
                 </div>
             )
         }
